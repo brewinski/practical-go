85bd80b63f664f9021d0fd2fcc8dd0685c5e9fa3
Chris Brewin <cbrewin93@gmail.com>
2023-12-22 22:53:07 +1000

    Add gzip decompression and calculate SHA1 checksum for a file

--- sha1/sha1.go
+++ sha1/sha1.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"compress/gzip"
+	"crypto/sha1"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-
+	fmt.Println(sha1Sum("./http.log.gz"))
 }
 
 func sha1Sum(fileName string) (string, error) {
@@ -17,5 +20,18 @@ func sha1Sum(fileName string) (string, error) {
 
 	defer file.Close()
 
-	return "", nil
+	r, err := gzip.NewReader(file)
+	if err != nil {
+		return "", fmt.Errorf("failed to open gzip reader: %w", err)
+	}
+
+	w := sha1.New()
+
+	if _, err := io.Copy(w, r); err != nil {
+		return "", fmt.Errorf("failed to copy to sha1: %w", err)
+	}
+
+	sig := w.Sum(nil)
+
+	return fmt.Sprintf("%x", sig), nil
 }
